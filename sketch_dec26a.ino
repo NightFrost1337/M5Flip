752f584b5466cf951c507088be12313047b7aaa4
NightFrost1337 <nightfrost2discord@outlook.fr>
2024-12-27 23:59:57 +0100

    added new menu

--- sketch_dec26a.ino
+++ sketch_dec26a.ino
@@ -1,71 +1,51 @@
 #include "M5StickCPlus2.h"
 
-#define MENU_SIZE 3
-
-const char* menuItems[MENU_SIZE] = {"Option 1", "Option 2", "Option 3"};
+String menuOptions[] = {"Home", "Config", "Info"};
 int currSelection = 0;
+int totalOptions = sizeof(menuOptions) / sizeof(menuOptions[0]);
 
 void setup() {
   M5.begin();
-  M5.Lcd.setRotation(1);  
-  M5.Lcd.clear();
-  M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.setTextSize(2);
+  M5.Lcd.setRotation(1);
+  M5.Lcd.fillScreen(TFT_BLACK);
+
+  displayMenu();
 }
 
 void loop() {
   M5.update();
 
   if (M5.BtnA.wasPressed()) {
-    selectOption();
-  }
-  if (M5.BtnB.wasPressed()) {
-    scrollDown();
+    currSelection = (currSelection - 1 + totalOptions) % totalOptions;
+    displayMenu();
   }
 
-  displayMenu();
-  delay(100);
+  if (M5.BtnB.wasPressed()) {
+    currSelection = (currSelection + 1) % totalOptions;
+    displayMenu();
+  }
 }
 
 void displayMenu() {
-  M5.Lcd.clear();
-  M5.Lcd.setCursor(10, 10);
-  M5.Lcd.setTextSize(3);
-  M5.Lcd.print("Menu");
-  
-  for (int i = 0; i < MENU_SIZE; i++) {
+  M5.Lcd.fillScreen(TFT_BLACK);
+
+  for (int i = 0; i < totalOptions; i++) {
+    int boxX = 20;
+    int boxY = 20 + i * 40;
+    int boxWidth = 200;
+    int boxHeight = 30;
+
     if (i == currSelection) {
-      M5.Lcd.setTextColor(YELLOW);
-      M5.Lcd.setTextSize(3);
+      M5.Lcd.fillRect(boxX, boxY, boxWidth, boxHeight, TFT_WHITE); 
+      M5.Lcd.drawRect(boxX, boxY, boxWidth, boxHeight, TFT_WHITE); 
+      M5.Lcd.setTextColor(TFT_BLACK);
     } else {
-      M5.Lcd.setTextColor(WHITE);
-      M5.Lcd.setTextSize(2);
+      M5.Lcd.drawRect(boxX, boxY, boxWidth, boxHeight, TFT_ORANGE); 
+      M5.Lcd.setTextColor(TFT_WHITE);
     }
 
-    M5.Lcd.setCursor(10, 40 + i * 30);
-    M5.Lcd.print(menuItems[i]);
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setCursor(boxX + 10, boxY + 5);
+    M5.Lcd.print(menuOptions[i]);
   }
-}
-
-void scrollDown() {
-  currSelection++;
-  if (currSelection >= MENU_SIZE) {
-    currSelection = 0;
-  }
-}
-
-void selectOption() {
-  M5.Lcd.clear();
-  M5.Lcd.setCursor(10, 10);
-  M5.Lcd.setTextSize(3);
-  
-  if (currSelection == 0) {
-    M5.Lcd.print("Option 1 selected");
-  } else if (currSelection == 1) {
-    M5.Lcd.print("Option 2 selected");
-  } else if (currSelection == 2) {
-    M5.Lcd.print("Option 3 selected");
-  }
-  
-  delay(1000);
 }
